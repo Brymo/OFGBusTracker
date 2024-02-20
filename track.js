b12961aad8b3c8f1528d35a9c03f11115b5eb5bc
Bryan Moh <bmoh@bmoh-2310.local>
2024-02-20 17:30:35 +1100

    Implemented time formatting

--- track.js
+++ track.js
@@ -4,13 +4,16 @@ const key =
 
 const ofgID = "10115610";
 const cbdID = "10111010";
+const numResults = 3;
+
+const usedID = cbdID;
 
 // ping TransportNSW with a request to get busData
 async function getBusData() {
   const date = getDate();
   const time = getTime();
 
-  const requestURL = `https://api.transport.nsw.gov.au/v1/tp/departure_mon?outputFormat=rapidJSON&coordOutputFormat=EPSG%3A4326&mode=direct&type_dm=stop&name_dm=${cbdID}&itdDate=${date}&itdTime=${time}&departureMonitorMacro=true&TfNSWDM=true&version=`;
+  const requestURL = `https://api.transport.nsw.gov.au/v1/tp/departure_mon?outputFormat=rapidJSON&coordOutputFormat=EPSG%3A4326&mode=direct&type_dm=stop&name_dm=${usedID}&itdDate=${date}&itdTime=${time}&departureMonitorMacro=true&TfNSWDM=true&version=`;
   const busData = await fetch(requestURL, {
     method: "GET",
     accept: "application/json",
@@ -19,27 +22,74 @@ async function getBusData() {
     },
   }).then((response) => response.json());
 
-  const trimmedBusData = busData.stopEvents.filter(stopEvent => stopEvent.isRealtimeControlled).map((stopEvent) => {
-    const { departureTimeBaseTimetable, departureTimePlanned,departureTimeEstimated,transportation } =
-      stopEvent;
-    return {
-      departureTimePlanned,
-      departureTimeBaseTimetable,
-      departureTimeEstimated,
-      busName: transportation.disassembledName,
-    };
-  });
+  const trimmedBusData = busData.stopEvents
+    .filter((stopEvent, index) => stopEvent.isRealtimeControlled && index < numResults)
+    .map((stopEvent) => {
+      const {
+        departureTimeBaseTimetable,
+        departureTimePlanned,
+        departureTimeEstimated,
+        transportation,
+      } = stopEvent;
+      return {
+        departureTimePlanned: isoToSydney(departureTimePlanned),
+        departureTimeBaseTimetable: isoToSydney(departureTimeBaseTimetable),
+        departureTimeEstimated: departureTimeEstimated && isoToSydney(departureTimeEstimated),
+        busName: transportation.disassembledName,
+      };
+    });
 
   return trimmedBusData;
 }
 
 function getDate() {
-  return "20240219";
+
+  const currentDate = new Date();
+  const dateElements = new Intl.DateTimeFormat("en-GB", {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    timeZone: "Australia/Sydney",
+    hourCycle: "h23",
+  })
+    .format(currentDate).split("/");
+
+  const shrunkDate = dateElements.reverse().join("");
+
+  return shrunkDate;
 }
 
 function getTime() {
-  return "1500";
+  const currentDate = new Date();
+  const dateElements = new Intl.DateTimeFormat("en-GB", {
+    timeStyle: "long",
+    timeZone: "Australia/Sydney",
+    hourCycle: "h23",
+  })
+    .format(currentDate)
+    .split(":");
+
+  const twentyFourHourTime = dateElements[0].concat(dateElements[1]);
+
+  return twentyFourHourTime;
 }
 
-getBusData()
-  .then(data => console.log(data));
+function isoToSydney(isoString){
+
+  function parseISOString(s) {
+    const b = s.split(/\D+/);
+    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+  }
+
+  const date = parseISOString(isoString);
+  const formattedDate = new Intl.DateTimeFormat("en-GB", {
+    timeStyle: "long",
+    timeZone: "Australia/Sydney",
+    hourCycle: "h23",
+  }).format(date).split(" ")[0];
+
+  return formattedDate;
+
+}
+
+getBusData().then((data) => console.log(data));
