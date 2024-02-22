95f612e8122bcfc67eeb9746f6b29d68b2bb798b
Bryan Moh <bmoh@bmoh-2310.local>
2024-02-22 15:33:12 +1100

    add server to hit api and then webpage that polls server for api data

--- track.js
+++ track.js
@@ -1,95 +1,11 @@
-// DELETE THIS
-const key =
-  "";
+run();
+setInterval(run, 10*1000);
 
-const ofgID = "10115610";
-const cbdID = "10111010";
-const numResults = 3;
-
-const usedID = cbdID;
-
-// ping TransportNSW with a request to get busData
-async function getBusData() {
-  const date = getDate();
-  const time = getTime();
-
-  const requestURL = `https://api.transport.nsw.gov.au/v1/tp/departure_mon?outputFormat=rapidJSON&coordOutputFormat=EPSG%3A4326&mode=direct&type_dm=stop&name_dm=${usedID}&itdDate=${date}&itdTime=${time}&departureMonitorMacro=true&TfNSWDM=true&version=`;
-  const busData = await fetch(requestURL, {
+async function run() {
+  await fetch("http://localhost:3000", {
     method: "GET",
     accept: "application/json",
-    headers: {
-      Authorization: `apikey ${key}`,
-    },
-  }).then((response) => response.json());
-
-  const trimmedBusData = busData.stopEvents
-    .filter((stopEvent, index) => stopEvent.isRealtimeControlled && index < numResults)
-    .map((stopEvent) => {
-      const {
-        departureTimeBaseTimetable,
-        departureTimePlanned,
-        departureTimeEstimated,
-        transportation,
-      } = stopEvent;
-      return {
-        departureTimePlanned: isoToSydney(departureTimePlanned),
-        departureTimeBaseTimetable: isoToSydney(departureTimeBaseTimetable),
-        departureTimeEstimated: departureTimeEstimated && isoToSydney(departureTimeEstimated),
-        busName: transportation.disassembledName,
-      };
-    });
-
-  return trimmedBusData;
-}
-
-function getDate() {
-
-  const currentDate = new Date();
-  const dateElements = new Intl.DateTimeFormat("en-GB", {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    timeZone: "Australia/Sydney",
-    hourCycle: "h23",
+    mode: "no-cors",
   })
-    .format(currentDate).split("/");
-
-  const shrunkDate = dateElements.reverse().join("");
-
-  return shrunkDate;
+    .then((response) => console.log(response))
 }
-
-function getTime() {
-  const currentDate = new Date();
-  const dateElements = new Intl.DateTimeFormat("en-GB", {
-    timeStyle: "long",
-    timeZone: "Australia/Sydney",
-    hourCycle: "h23",
-  })
-    .format(currentDate)
-    .split(":");
-
-  const twentyFourHourTime = dateElements[0].concat(dateElements[1]);
-
-  return twentyFourHourTime;
-}
-
-function isoToSydney(isoString){
-
-  function parseISOString(s) {
-    const b = s.split(/\D+/);
-    return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
-  }
-
-  const date = parseISOString(isoString);
-  const formattedDate = new Intl.DateTimeFormat("en-GB", {
-    timeStyle: "long",
-    timeZone: "Australia/Sydney",
-    hourCycle: "h23",
-  }).format(date).split(" ")[0];
-
-  return formattedDate;
-
-}
-
-getBusData().then((data) => console.log(data));
